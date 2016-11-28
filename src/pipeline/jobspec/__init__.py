eb504611b29d5e23509c85af223a6b608946ace5
Lixiang Ao <aolixiang@gmail.com>
2016-11-28 09:49:26 -0800

    fix init file

--- src/pipeline/jobspec/__init__.py
+++ src/pipeline/jobspec/__init__.py
@@ -1,3 +1,11 @@
+from .generator import *
+from .validator import *
+from .parser    import *
+from .job_spec  import *
+
+import json
+import simplejson
+
 from .generator2 import *
 from .job_coordinator import *
 
