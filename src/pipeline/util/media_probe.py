be6bb6e39ad16043e9056961c881ab194925d4dc
Lixiang Ao <aolixiang@gmail.com>
2017-02-14 23:13:32 -0800

    Update media_probe

--- src/pipeline/util/media_probe.py
+++ src/pipeline/util/media_probe.py
@@ -3,13 +3,20 @@
 import os
 import subprocess
 
+import boto3
+
 from .s3signurl import sign
 
 def get_signed_URI(URI):
     if URI.startswith('s3://'):
-        return sign(URI.split('/')[2], '/'.join(split('/')[3:]), os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], https=True, expiry=86400)
+        return sign(URI.split('/')[2], '/'.join(URI.split('/')[3:]), os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], https=True, expiry=86400)
     else:
         return URI
+
+
+def get_all_info(URI):
+    results = subprocess.Popen(["ffprobe", URI], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    return results.stdout.readlines()
 
 
 def get_duration(URI):
@@ -18,7 +25,17 @@ def get_duration(URI):
     return sum(x * int(t) for x, t in zip([3600, 60, 1], iso_time.split('.')[0].split(":")))+float('.'+iso_time.split('.')[1])
 
 
-def get_all_info(URI):
-    results = subprocess.Popen(["ffprobe", URI], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-    return results.stdout.readlines()
+def get_nframes(URI, suffix=''): # a wildcard suffix
+    if URI.startswith('s3://'):
+        client = boto3.client('s3')
+        count = 0
+        marker = ''
+        while True:
+            objs = client.list_objects_v2(Bucket=URI.split('/')[2], Prefix='/'.join(URI.split('/')[3:]), StartAfter=marker)
+            count += len(filter(lambda x: x['Key'].endswith(suffix) and not x['Key'].endswith('/'), objs['Contents']))
+            marker = objs['Contents'][len(objs['Contents'])-1]['Key']
+            if not objs['IsTruncated']:
+                break
+        return count
+    return None
 
