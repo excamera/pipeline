8287470b3de74d804f752204c02bc29dff4ba248
Lixiang Ao <aolixiang@gmail.com>
2017-07-08 10:06:09 -0700

    Add ladder scheduler

--- src/pipeline/taskspec/scheduler.py
+++ src/pipeline/taskspec/scheduler.py
@@ -2,7 +2,7 @@
 import Queue
 
 import logging
-
+import threading
 import time
 
 from libmu import tracker
@@ -111,6 +111,75 @@ class BarrierScheduler(object):
 
             if buffer_empty and deliver_empty and len(tasks) == 0:
                 break
+
+            if time.time() > last_print+1:
+                print_task_states(tasks)
+                last_print = time.time()
+            time.sleep(0.001)
+            # sleep to avoid spinning, we can use notification instead, but so far, this works.
+            # it may increase overall latency by at most n*0.001 second, where n is length of pipeline
+
+        logging.info('finish scheduling pipeline')
+
+
+class LadderScheduler(object):
+    """A scheduler that scans every stage for events and submits at most 200 tasks each second"""
+    @classmethod
+    def schedule(cls, pipeline):
+        logging.info('start scheduling pipeline')
+        last_fill = 0
+        quota = 0
+        last_print = 0
+        tasks = []
+        gen = None
+        while True:
+            buffer_empty = True
+            deliver_empty = True
+            for key, stage in pipeline.stages.iteritems():
+                stage.deliver_func = default_deliver_func if stage.deliver_func is None else stage.deliver_func
+                if not stage.buffer_queue.empty():
+                    buffer_empty = False
+                    stage.deliver_func(stage.buffer_queue, stage.deliver_queue)
+
+            for key, stage in pipeline.stages.iteritems():
+                if not stage.deliver_queue.empty():
+                    deliver_empty = False
+                    break
+
+            if buffer_empty and deliver_empty and len(tasks) == 0:
+                break
+
+            if time.time() > last_fill + 1:
+                quota = 50
+                last_fill = time.time()
+
+            def task_gen():
+                for key, stage in pipeline.stages.iteritems():
+                    while not stage.deliver_queue.empty():
+                        t = tracker.Task(stage.lambda_function, stage.init_state, stage.deliver_queue.get(), stage.downstreams, stage.event)
+                        yield t
+
+            while quota > 0:
+                try:
+                    if gen is None:
+                        gen = task_gen()
+                    t = gen.next()
+                    tasks.append(t)
+                    tracker.Tracker.submit(t)
+                    logging.debug('submitted a task: '+str(t))
+                    quota -= 1
+                except StopIteration:
+                    gen = task_gen()
+                    break
+
+            error_tasks = [t for t in tasks if isinstance(t.current_state, ErrorState)]
+            if len(error_tasks) > 0:
+                logging.error(str(len(error_tasks))+" tasks failed: ")
+                for et in error_tasks:
+                    logging.error(et.current_state.str_extra())
+                raise Exception(str(len(error_tasks))+" tasks failed")
+            tasks = [t for t in tasks if not isinstance(t.current_state, TerminalState)]
+
 
             if time.time() > last_print+1:
                 print_task_states(tasks)
