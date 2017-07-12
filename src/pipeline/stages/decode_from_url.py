cd78c3b660be17c404fc08cf43dce255026f363e
Lixiang Ao <aolixiang@gmail.com>
2017-07-12 14:44:15 -0700

    decode_from_url should be non-linear, may produce no result

--- src/pipeline/stages/decode_from_url.py
+++ src/pipeline/stages/decode_from_url.py
@@ -2,37 +2,73 @@
 import logging
 
 import libmu.util
-from libmu import tracker, TerminalState, CommandListState, ForLoopState, OnePassState, ErrorState
+from libmu import tracker, TerminalState, CommandListState, ForLoopState, OnePassState, ErrorState, IfElseState
 from stages.util import default_trace_func
 
 
-class FinalState(TerminalState):
-    extra = "(finished)"
-
-
-class EmitState(CommandListState):
-    extra = "(emit output)"
-    nextState = FinalState
-    commandlist = [ (None, "quit:")
-                  ]
+class FinalState(OnePassState):
+    extra = "(sending quit)"
+    expect = None
+    command = "quit:"
+    nextState = TerminalState
 
     def __init__(self, prevState):
-        super(EmitState, self).__init__(prevState, trace_func=default_trace_func)
-        out_queue = prevState.out_queue
-        out_key = prevState.out_key
+        super(FinalState, self).__init__(prevState, trace_func=default_trace_func)
 
-        out_event = {'key': out_key}
-        out_queue['frames'].put({'metadata': self.in_events['metadata'], 'frames': out_event})
+
+class ConfirmEmitState(OnePassState):
+    extra = "(confirm emit)"
+    expect = 'OK:EMIT'
+    command = None
+    nextState = FinalState
+
+    def __init__(self, prevState):
+        super(ConfirmEmitState, self).__init__(prevState, trace_func=default_trace_func)
+        self.out_queue = prevState.out_queue
+        self.out_key = prevState.out_key
+
+    def post_transition(self):
+        out_event = {'key': self.out_key}
+        self.out_queue['frames'].put({'metadata': self.in_events['metadata'], 'frames': out_event})
+        return self.nextState(self)  # don't forget this
+
+
+class TryEmitState(OnePassState):
+    extra = "(emit output)"
+    expect = None
+    command = 'emit:##TMPDIR##/out_0 {out_key}'
+    nextState = ConfirmEmitState
+
+    def __init__(self, prevState):
+        super(TryEmitState, self).__init__(prevState, trace_func=default_trace_func)
+        self.out_queue = prevState.out_queue
+        self.out_key = prevState.out_key
+        params = {'out_key': self.out_key}
+        self.command = self.command.format(**params)
+
+
+class CheckOutputState(IfElseState):
+    extra = "(check output)"
+    expect = 'OK:RETVAL('
+    consequentState = TryEmitState
+    alternativeState = FinalState
+
+    def testfn(self):
+        return self.messages[-1].startswith('OK:RETVAL(0)')
+
+    def __init__(self, prevState):
+        super(CheckOutputState, self).__init__(prevState, trace_func=default_trace_func)
+        self.out_queue = prevState.out_queue
+        self.out_key = prevState.out_key
 
 
 class RunState(CommandListState):
     extra = "(run)"
-    nextState = EmitState
+    nextState = CheckOutputState
     commandlist = [ (None, 'run:mkdir -p ##TMPDIR##/out_0/')
                   , ('OK:RETVAL(0)', 'run:./ffmpeg -y -ss {starttime} -t {duration} -i "{URL}" -f image2 -c:v png '
                                     '-start_number 1 ##TMPDIR##/out_0/%08d.png')
-                  , ('OK:RETVAL(0)', 'emit:##TMPDIR##/out_0 {out_key}')
-                  , ('OK:EMIT', None)
+                  , ('OK:RETVAL(0)', 'run:test `find ##TMPDIR##/out_0/ -name "*png" | wc -l` -gt 0')
                     ]
 
     def __init__(self, prevState):
