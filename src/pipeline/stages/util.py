596b1410961eee3366e8fca3a8dae8a1fadbbd43
Lixiang Ao <aolixiang@gmail.com>
2017-08-09 15:18:35 -0700

    Allow different lambda functions.
    User can specify lambda function for each stage in the pipespec.
    Note this is not compatible with old lambda functions.

--- src/pipeline/stages/util.py
+++ src/pipeline/stages/util.py
@@ -9,11 +9,12 @@ def get_default_event():
     return {
         "mode": 1
         , "port": settings['tracker_port']
-        , "addr": None  # server_launch will fill this in for us
+        , "addr": None  # tracker will fill this in for us
         , "nonblock": 0
         # , 'cacert': libmu.util.read_pem(settings['cacert_file']) if 'cacert_file' in settings else None
         , 'srvcrt': libmu.util.read_pem(settings['srvcrt_file']) if 'srvcrt_file' in settings else None
         , 'srvkey': libmu.util.read_pem(settings['srvkey_file']) if 'srvkey_file' in settings else None
+        , 'lambda_function': settings['default_lambda_function']
     }
 
 
