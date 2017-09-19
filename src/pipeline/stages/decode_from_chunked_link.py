afd2603579b94327f857ff588989885587bfa3c0
Lixiang Ao <liao@eng.ucsd.edu>
2017-09-19 16:13:39 -0700

    Add format selector
    use condition filter in brackets described in https://github.com/rg3/youtube-dl#format-selection

--- src/pipeline/stages/decode_from_chunked_link.py
+++ src/pipeline/stages/decode_from_chunked_link.py
@@ -29,7 +29,7 @@ class ConfirmEmitState(OnePassState):
 
     def post_transition(self):
         self.emit_event('frames', {'metadata': self.in_events['chunked_link']['metadata'], 'key': self.local['out_key']
-                                   , 'nframes': self.local['output_count']})
+            , 'nframes': self.local['output_count']})
         return self.nextState(self)  # don't forget this
 
 
@@ -63,9 +63,10 @@ class RunState(CommandListState):
     extra = "(run)"
     nextState = CheckOutputState
     commandlist = [(None, 'run:mkdir -p ##TMPDIR##/out_0/')
-        , ('OK:RETVAL(0)', 'run:./youtube-dl -f "(mp4)" --get-url {URL} 2>/dev/null | head -n1 | xargs -IPLACEHOLDER '
-                           './ffmpeg -y -ss {starttime} -i PLACEHOLDER -frames {frames} -f image2 -c:v png '
-                           '-start_number 1 ##TMPDIR##/out_0/%08d.png')
+        , ('OK:RETVAL(0)',
+           'run:./youtube-dl --get-url {URL} -f "{selector}" 2>/dev/null | head -n1 | xargs -IPLACEHOLDER '
+           './ffmpeg -y -ss {starttime} -i PLACEHOLDER -frames {frames} -f image2 -c:v png '
+           '-start_number 1 ##TMPDIR##/out_0/%08d.png')
         , ('OK:RETVAL(0)', 'run:find ##TMPDIR##/out_0/ -name "*png" | wc -l')
                    # result will be used in next state
                    ]
@@ -75,7 +76,9 @@ class RunState(CommandListState):
 
         params = {'starttime': self.in_events['chunked_link']['starttime'],
                   'frames': self.in_events['chunked_link']['frames'],
-                  'URL': self.in_events['chunked_link']['key'], 'out_key': self.local['out_key']}
+                  'URL': self.in_events['chunked_link']['key'],
+                  'selector': self.in_events['chunked_link']['selector'],
+                  'out_key': self.local['out_key']}
         logging.debug('params: ' + str(params))
         self.commands = [s.format(**params) if s is not None else None for s in self.commands]
 
@@ -84,13 +87,14 @@ class InitState(CommandListState):
     extra = "(init)"
     nextState = RunState
     commandlist = [("OK:HELLO", "seti:nonblock:0")
-        # , "run:rm -rf /tmp/*"
+                   # , "run:rm -rf /tmp/*"
         , "run:mkdir -p ##TMPDIR##"
         , None
                    ]
 
     def __init__(self, prevState, in_events, emit_event, config):
-        super(InitState, self).__init__(prevState, in_events=in_events, emit_event=emit_event, config=config, trace_func=default_trace_func)
+        super(InitState, self).__init__(prevState, in_events=in_events, emit_event=emit_event, config=config,
+                                        trace_func=default_trace_func)
         self.local['out_key'] = settings['storage_base'] + in_events['chunked_link']['metadata'][
             'pipe_id'] + '/decode/' + libmu.util.rand_str(16) + '/'
         logging.debug('in_events: ' + str(in_events))
--- src/pipeline/stages/parallelize_link.py
+++ src/pipeline/stages/parallelize_link.py
@@ -37,15 +37,17 @@ class EmitState(OnePassState):
         overlap = config.get('overlap', 0)
 
         i = 0
-        while i * (float(framesperchunk) - overlap) / metadata['fps'] < self.local['duration']:  # actual parallelizing here
-            metacopy = metadata.copy()
+        while i * (float(framesperchunk) - overlap) / metadata['fps'] < self.local['duration']:
+            # actual parallelizing here
+            newmeta = metadata.copy()
             starttime = i * (framesperchunk - overlap) / metadata['fps']
-            metacopy['lineage'] = str(i+1)
-            metacopy['chunk_duration'] = float(framesperchunk) / metadata['fps']
-            self.emit_event('chunked_link', {'metadata': metacopy,
-                                       'key': self.in_events['video_link']['key'],
-                                       'starttime': starttime,
-                                       'frames': framesperchunk})
+            newmeta['lineage'] = str(i + 1)
+            newmeta['chunk_duration'] = float(framesperchunk) / metadata['fps']
+            self.emit_event('chunked_link', {'metadata': newmeta,
+                                             'key': self.in_events['video_link']['key'],
+                                             'selector': self.local['selector'],
+                                             'starttime': starttime,
+                                             'frames': framesperchunk})
             i += 1
         return self.nextState(self)  # don't forget this
 
@@ -63,32 +65,38 @@ class GetOutputState(OnePassState):
         output = json.loads(get_output_from_message(self.messages[-1]))
         self.local['duration'] = output['duration']
         self.in_events['video_link']['metadata']['fps'] = output['fps']
+        logging.debug("selected format: %s, fps: %d" % (output['format'], output['fps']))
         return self.nextState(self)  # don't forget this
 
 
 class RunState(CommandListState):
     extra = "(run)"
     nextState = GetOutputState
-    commandlist = [(None, 'run:./youtube-dl -j {link} 2>/dev/null')
-                   # output will be used in latter states
+    commandlist = [(None, 'run:./youtube-dl -j {link} -f "{selector}" > ##TMPDIR##/video_info')
+        , ('OK:RETVAL(0', 'run:cat ##TMPDIR##/video_info')
                    ]
 
     def __init__(self, prevState):
         super(RunState, self).__init__(prevState)
-
-        params = {'link': self.in_events['video_link']['key']}
+        default_selector = 'mp4[fps>=0]{user_filter}/webm[fps>=0]{user_filter}/ogg[fps>=0]{user_filter}/' \
+                           'flv[fps>=0]{user_filter}/3gp[fps>=0]{user_filter}'  # acceptable formats in preference order
+        selector = default_selector.format(**{'user_filter': self.config.get('filter', '')})
+        self.local['selector'] = selector
+        params = {'link': self.in_events['video_link']['key'],
+                  'selector': selector}
         self.commands = [s.format(**params) if s is not None else None for s in self.commands]
 
 
 class InitState(CommandListState):
     extra = "(init)"
     nextState = RunState
-    commandlist = [ ("OK:HELLO", "seti:nonblock:0")
-                  # , "run:rm -rf /tmp/*"
-                  , "run:mkdir -p ##TMPDIR##"
-                  , None
-                  ]
+    commandlist = [("OK:HELLO", "seti:nonblock:0")
+                   # , "run:rm -rf /tmp/*"
+        , "run:mkdir -p ##TMPDIR##"
+        , None
+                   ]
 
     def __init__(self, prevState, in_events, emit_event, config):
-        super(InitState, self).__init__(prevState, in_events=in_events, emit_event=emit_event, config=config, trace_func=default_trace_func)
-        logging.debug('in_events: '+str(in_events))
+        super(InitState, self).__init__(prevState, in_events=in_events, emit_event=emit_event, config=config,
+                                        trace_func=default_trace_func)
+        logging.debug('in_events: ' + str(in_events))
