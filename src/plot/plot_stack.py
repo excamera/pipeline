265447b3aec56b84e2f9b39b7afd46e718b455b8
Lixiang Ao <aolixiang@gmail.com>
2017-12-22 18:19:50 -0800

    update plot for protobuf-based job log

--- src/plot/plot_stack.py
+++ src/plot/plot_stack.py
@@ -1,19 +1,25 @@
+from __future__ import print_function
 import sys
+import os
 import pdb
 import re
 
 import matplotlib.pyplot as plt
 
 from util import preprocess
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))+'/../../external/mu/src/lambdaize/libmu/')
 
-cmd_of_interest = ('seti', 'request', 'run:./ffmpeg', 'emit', 'quit', 'collect', 'run:./youtube-dl', 'run:tar')
+import joblog_pb2
+
+fields = ('ts', 'lineage', 'op', 'msg', 'stage', 'worker_called', 'num_frames')
+cmds = ('seti', 'invocation', 'request', 'run:./ffmpeg', 'emit', 'quit', 'collect', 'run:./youtube-dl', 'run:tar')
 
 def plot_stack(lines, chunk_length=None, ystart=None):
-    data = preprocess(lines) # kwargs: tuple cmd_of_interest, bool send_only
+    data = preprocess(lines, cmd_of_interest=cmds, send_only=True) # empty string cmd_of_interest means all cmds
     values = data.values()
     for j in reversed(xrange(0, len(values[0]))):
-        ts = [r[j][0] for r in values]
-        label = values[0][j-1][1]
+        ts = [r[j]['ts'] for r in values]
+        label = values[0][j-1]['msg'][:10]
         if chunk_length:
             xscale = [chunk_length * x for x in xrange(1, len(data)+1)]
         else:
@@ -43,9 +49,37 @@ def plot_stack(lines, chunk_length=None, ystart=None):
 
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        lines = f.readlines()
+    if len(sys.argv) < 2:
+        print('usage: %s log_file [chunk_length [start_playing_time]]' % sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    
+    if sys.argv[1].endswith('.csv'):
+        with open(sys.argv[1], 'r') as f:
 
+            def define_fields(line):
+                fields = line.split(', ')
+                if len(fields) == 2:
+                    return {'ts': float(fields[0]), 'msg': fields[1]}
+                elif len(fields) >= 4:
+                    return {'ts': float(fields[0]), 'lineage': fields[1], 'op': fields[2], 'msg': ', '.join(fields[3:])}
+                else:
+                    raise Exception('undefined fields: %s' % line)
+
+            lines = [define_fields(l.strip()) for l in f.readlines()]
+    else:
+        with open(sys.argv[1], 'rb') as f:
+            jl = joblog_pb2.JobLog()
+            jl.ParseFromString(f.read())
+
+            def define_fields(line):
+                ret = {}
+                for a in fields:
+                    if hasattr(line, a):
+                        ret[a] = getattr(line, a)
+                return ret
+
+            lines = [define_fields(l) for l in jl.record]
+            
     chunk_length = None
     ystart = None
     if len(sys.argv) >= 3:
--- src/plot/util.py
+++ src/plot/util.py
@@ -4,51 +4,48 @@ __all__ = ['plot_stack']
 
 
 def preprocess(lines, cmd_of_interest="", send_only=False):
-    assert lines[0].split(',')[1].strip() == 'starting pipeline'
-    assert lines[-1].split(',')[1].strip() == 'pipeline finished'
+    assert lines[0]['msg'] == 'starting pipeline' or lines[0]['msg'] == 'start pipeline' 
+    assert lines[-1]['msg'] == 'pipeline finished' or lines[-1]['msg'] == 'finish pipeline' 
 
-    start_ts = float(lines[0].split(',')[0])
+    start_ts = lines[0]['ts']
     data = OrderedDict()
     for i in xrange(1, len(lines)-1):  # first, last lines excluded
-        fields = lines[i].split(',', 4)
-        ts = float(fields[0].strip())
-        lineage = fields[1].strip()
-        op = fields[2].strip()
-        msg = fields[3].strip()
-        if lineage == '0' or (send_only and op != 'send'):
+        l = lines[i]
+        if l['lineage'] == '0' or (send_only and l['op'] != 'send'):
             continue
-        if lineage not in data:
-            data[lineage] = []
-        if msg.startswith(cmd_of_interest):
-            data[lineage].append((ts-start_ts, op+':'+msg[:10]))
+        if l['lineage'] not in data:
+            data[l['lineage']] = []
+        if l['msg'].startswith(cmd_of_interest):
+            l['ts'] = l['ts']-start_ts # relative timestamp
+            data[l['lineage']].append(l)
 
     return data
 
 
-def precise_time(lines, cmd_of_interest):
-    """
-        assumption: recv msg is right after the corresponding send msg (for the same lineage)
-    """
-    assert lines[0].split(',')[1].strip() == 'starting pipeline'
-    assert lines[-1].split(',')[1].strip() == 'pipeline finished'
-
-    data = dict()
-    current = dict()
-    for i in xrange(1, len(lines)-1):  # first, last lines excluded
-        fields = lines[i].split(',', 4)
-        ts = float(fields[0].strip())
-        lineage = fields[1].strip()
-        op = fields[2].strip()
-        msg = fields[3].strip()
-        if lineage == '0':
-            continue
-        if lineage in current:
-            start_record = current.pop(lineage)
-            lineage_records = data.get(lineage, [])
-            lineage_records.append({'msg': start_record['msg'][:10], 'duration': ts - start_record['ts']})
-            data[lineage] = lineage_records
-            continue
-        if op == 'send' and msg.startswith(cmd_of_interest):
-            current[lineage] = {'ts': ts, 'msg': msg}
-
-    return data  # not ordered
+#def precise_time(lines, cmd_of_interest):
+#    """
+#        assumption: recv msg is right after the corresponding send msg (for the same lineage)
+#    """
+#    assert lines[0].split(',')[1].strip() == 'starting pipeline'
+#    assert lines[-1].split(',')[1].strip() == 'pipeline finished'
+#
+#    data = dict()
+#    current = dict()
+#    for i in xrange(1, len(lines)-1):  # first, last lines excluded
+#        fields = lines[i].split(',', 4)
+#        ts = float(fields[0].strip())
+#        lineage = fields[1].strip()
+#        op = fields[2].strip()
+#        msg = fields[3].strip()
+#        if lineage == '0':
+#            continue
+#        if lineage in current:
+#            start_record = current.pop(lineage)
+#            lineage_records = data.get(lineage, [])
+#            lineage_records.append({'msg': start_record['msg'][:10], 'duration': ts - start_record['ts']})
+#            data[lineage] = lineage_records
+#            continue
+#        if op == 'send' and msg.startswith(cmd_of_interest):
+#            current[lineage] = {'ts': ts, 'msg': msg}
+#
+#    return data  # not ordered
